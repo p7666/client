cc151b58a050a58b9b465596f4feb19dc1d1278c
p7666 <priyankasonawane7666@gmail.com>
2025-03-09 14:03:59 +0530

    Update RecipeCard.jsx

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
+import React from "react";
 
 const RecipeCard = ({ recipe }) => {
   return (
-    <div className="card">
-      <img src={recipe.image} alt={recipe.title} className="card-img-top" />
+    <div className="card mb-4 shadow-sm">
+      {/* Recipe Image */}
+      <img
+        src={recipe.imageUrl}
+        alt={recipe.name}
+        className="card-img-top"
+        style={{ height: "200px", objectFit: "cover" }}
+      />
+
+      {/* Card Body */}
       <div className="card-body">
-        <h5 className="card-title">{recipe.title}</h5>
-        <p>Cooking Time: {recipe.cookingTime} mins</p>
-        <Link to={`/recipe/${recipe._id}`} className="btn btn-primary">
-          View
-        </Link>
+        <h5 className="card-title">{recipe.name}</h5>
+        <p className="card-text"><strong>Cooking Time:</strong> {recipe.cookingTime} mins</p>
+        
+        {/* Ingredients */}
+        <p className="card-text">
+          <strong>Ingredients:</strong> {recipe.ingredients.join(", ")}
+        </p>
+
+        {/* Instructions */}
+        <p className="card-text">
+          <strong>Instructions:</strong> {recipe.instructions}
+        </p>
       </div>
     </div>
   );
